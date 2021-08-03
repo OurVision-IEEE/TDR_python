103ed3d715e5596d3a4dc797e5853f0ad6591b60
tezz-io <matblockgreninja@gmail.com>
2021-08-03 20:14:29 +0530

    Added SHA256 checksum verification for downloaded things

---
+++ tdr/network.py
@@ -0,0 +1,45 @@
+import os
+import hashlib
+import urllib.request
+import urllib.parse
+
+def get_default_cache_dir():
+    return os.environ.get('KERAS_OCR_CACHE_DIR', os.path.expanduser(os.path.join('~',
+                                                                              '.keras-ocr')))
+def sha256sum(filename):
+    """Compute the sha256 hash for a file."""
+    h = hashlib.sha256()
+    b = bytearray(128 * 1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        for n in iter(lambda: f.readinto(mv), 0):
+            h.update(mv[:n])
+    return h.hexdigest()
+
+def download_and_verify(url, sha256=None, cache_dir=None, verbose=True, filename=None):
+    """Download a file to a cache directory and verify it with a sha256
+    hash.
+    Args:
+        url: The file to download
+        sha256: The sha256 hash to check. If the file already exists and the hash
+            matches, we don't download it again.
+        cache_dir: The directory in which to cache the file. The default is
+            `~/.keras-ocr`.
+        verbose: Whether to log progress
+        filename: The filename to use for the file. By default, the filename is
+            derived from the URL.
+    """
+    if cache_dir is None:
+        cache_dir = get_default_cache_dir()
+    if filename is None:
+        filename = os.path.basename(urllib.parse.urlparse(url).path)
+    filepath = os.path.join(cache_dir, filename)
+    os.makedirs(os.path.split(filepath)[0], exist_ok=True)
+    if verbose:
+        print('Looking for ' + filepath)
+    if not os.path.isfile(filepath) or (sha256 and sha256sum(filepath) != sha256):
+        if verbose:
+            print('Downloading ' + filepath)
+        urllib.request.urlretrieve(url, filepath)
+    assert sha256 is None or sha256 == sha256sum(filepath), 'Error occurred verifying sha256.'
+    return filepath
