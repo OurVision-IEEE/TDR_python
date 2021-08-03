25d31d8e4ab65581c5caf186d3e51f2a4f956f02
tezz-io <matblockgreninja@gmail.com>
2021-08-03 19:54:14 +0530

    Added swish.py for activation

---
+++ tdr/swish.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow import keras
+
+def swish(x, beta=1):
+    return x * keras.backend.sigmoid(beta * x)
+
+def setup_swish():
+    keras.utils.get_custom_objects().update({'swish': keras.layers.Activation(swish)})
