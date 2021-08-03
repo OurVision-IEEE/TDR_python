9117a8dc828aa1e3d79b67f5f07e3548cba12744
tezz-io <matblockgreninja@gmail.com>
2021-08-03 20:20:02 +0530

    Added main.py to add weights_dict and load_weights to the model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tdr.build_model import build_model
 from tdr.hyperparameters import DEFAULT_ALPHABET, DEFAULT_BUILD_PARAMS, PRETRAINED_WEIGHTS
+from tdr.network import download_and_verify
 
 if __name__ == "__main__":
     build_params = DEFAULT_BUILD_PARAMS
@@ -7,3 +8,13 @@ if __name__ == "__main__":
     blank_index = len(alphabets)
 
     model, prediction_model = build_model(alphabet=alphabets, **build_params)
+
+    weights_dict = PRETRAINED_WEIGHTS['kurapan']
+
+    model.load_weights(
+        download_and_verify(
+            url=weights_dict['weights']['top']['url'],
+            filename=weights_dict['weights']['top']['filename'],
+            sha256=weights_dict['weights']['top']['sha256']
+        )
+    )
