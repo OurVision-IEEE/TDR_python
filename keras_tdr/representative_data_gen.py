d8afe0678d8f4f03b1f5d7225a21b27ff7a4c83e
tezz-io <matblockgreninja@gmail.com>
2021-08-03 20:44:24 +0530

    Added representative_data_gen module to generate represent_data

---
+++ keras_tdr/representative_data_gen.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+import cv2
+
+def representative_data_gen(dataset_path):
+    for file in os.listdir(dataset_path):
+        image_path = dataset_path + file
+        input_data = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        input_data = cv2.resize(input_data, (200, 31))
+        input_data = input_data[np.newaxis]
+        input_data = np.expand_dims(input_data, 3)
+        input_data = input_data.astype('float32')/255
+        yield [input_data]
